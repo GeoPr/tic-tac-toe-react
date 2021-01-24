c09aa1c2397ef2d50d1111a0202afeb79b8d78e7
George <klassnoyparen@gmail.com>
2021-01-24 11:19:53 +0300

    _

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { StateProvider } from './state/state'
-import App from './components/App'
-import './index.scss'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './index.scss';
 
-render(
-  <StateProvider>
-    <App />
-  </StateProvider>,
-  document.getElementById('root'),
-)
+render(<App />, document.getElementById('root'));
